f1f72c26829b01695dad2007b2b37016f5239631
Samanwoy Saha <samanwoy.web@gmail.com>
2025-05-22 10:27:20 +0600

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(String dockerHubUsername, String imageName, String imageTag, ){
+def call(String dockerHubUsername, String imageName, String imageTag){
   sh "docker build -t ${dockerHubUsername}/${imageName}:${imageTag} ."
 }
